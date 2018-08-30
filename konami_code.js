b1f13aa3d88313ddc623ade58ca84a43fd6665c5
RebGov <becci.govert@gmail.com>
2018-08-30 08:16:28 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,32 @@ const codes = [
   "a"
 ];
 
+// function init() {
+//   // your code here
+//   let index = 0;
+//   document.body.eventListener("keydown", (event)=> {
+//     const key = event.key
+//     index = (codes[index]=== key) ? index++ :0
+//
+//       if (key === code.length){
+//         window.alert("Congratulations!!");
+//         index = 0;
+//       }
+//
+//   });
+// }
 function init() {
-  // your code here
+  let index = 0
+  document.body.addEventListener("keydown", (event) => {
+    const key = event.key
+
+    index = (codes[index] === key) ? ++index : 0
+
+
+    if (index === codes.length) {
+      window.alert("Hurray!");
+      index = 0
+    }
+
+  });
 }
