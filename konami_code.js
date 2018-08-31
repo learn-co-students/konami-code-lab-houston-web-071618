3d603f0ef9814159f01606762b9338409b1b9001
e-barr <github email address>
2018-08-31 08:55:03 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+document.body.addEventListener('keydown', e => init(e))
+let i;
+
+function init(e) {
+  if (i) {
+    i = i
+  } else {
+    i = 0
+  }
+
+  const key = e.key
+
+  if (key === codes[i] && i === codes.length - 1) {
+    i = 0
+    alert('Yay!')
+  } else if (key === codes[i]) {
+    i++
+  } else {
+    i = 0
+  }
+
 }
