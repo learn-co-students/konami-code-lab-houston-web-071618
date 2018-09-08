652fb8210f5094015aab0b27d2c5edb6b01ab789
pabloargueta <pabloargueta27@gmail.com>
2018-09-08 13:45:32 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,18 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+
+  document.body.addEventListener('keydown', (e) => {
+    if (e.key === codes[index]) {
+      index++;
+
+      if (index === codes.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
